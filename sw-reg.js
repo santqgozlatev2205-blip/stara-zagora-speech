fd9c3eece5295500448ac4f25655e045fe395d67
KIRIL KIRILOV <santqgozlatev2205@gmail.com>
2026-01-07 00:31:43 +0200

    sw-reg.js

--- sw-reg.js
+++ sw-reg.js
@@ -1,6 +1,2 @@
+// sw-reg.js
 navigator.serviceWorker.register('sw.js');
-window.addEventListener('load', async () => {
-  const sw = await navigator.serviceWorker.ready;
-  const perm = await Notification.requestPermission();
-  if (perm === 'granted') await sw.pushManager.subscribe({userVisibleOnly: true});
-});
