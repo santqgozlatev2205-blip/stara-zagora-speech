51948c1325114b27c433605d008c3c651a03a833
KIRIL KIRILOV <santqgozlatev2205@gmail.com>
2026-01-07 00:20:24 +0200

    sw-reg.js

--- sw-reg.js
+++ sw-reg.js
@@ -1,8 +1,6 @@
-// регистрираме SW + поискаме известия
 navigator.serviceWorker.register('sw.js');
-
 window.addEventListener('load', async () => {
-  const sw = await navigator.serviceReady;
+  const sw = await navigator.serviceWorker.ready;
   const perm = await Notification.requestPermission();
-  if (perm === 'granted') await sw.pushManager.subscribe({userVisibleOnly:true});
+  if (perm === 'granted') await sw.pushManager.subscribe({userVisibleOnly: true});
 });
